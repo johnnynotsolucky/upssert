3bac2129b058660d8cde5d67b4a57039c9f2dbe7
Tyrone T <tyronetudehope@gmail.com>
2016-11-09 14:32:14 +0000

    Transpose results from httpstat

--- index.js
+++ index.js
@@ -6,9 +6,32 @@ module.exports = main({
     {
       name: 'Something',
       request: {
-        url: 'http://httpbin.org/get',
+        url: 'https://httpbin.org/get?a=b',
         method: 'GET'
+      },
+      response: {
+        'content-type': 'application/json',
+        'status': {
+          'is-at-least': 200,
+          'is-below': 300,
+        },
+        'response-time': {
+          'is-below': 400
+        }
       }
-    }
+    },
   ]
+}).then((results) => {
+  results.forEach((output) => {
+    console.log(output.result);
+  });
+})
+.catch((err) => {
+  if (err.name === 'ValidationError') {
+    console.log('ValidationError');
+  } else if (err.name === 'AssertionError') {
+    console.log(err.message);
+  } else {
+    console.log(err);
+  }
 });
