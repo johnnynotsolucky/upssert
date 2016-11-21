263424ca247795450aa7a7a1dc195f5ef07034fb
Tyrone T <tyronetudehope@gmail.com>
2016-11-21 19:25:19 +0000

    Return false when package.json cannot be read; Fixes #1

--- dist/bin/read-json-file.js
+++ dist/bin/read-json-file.js
@@ -11,7 +11,13 @@ var _fs2 = _interopRequireDefault(_fs);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (file) {
-  var fileContents = _fs2.default.readFileSync(file);
-  var jsonContent = JSON.parse(fileContents);
-  return jsonContent;
+  var result = void 0;
+  try {
+    var fileContents = _fs2.default.readFileSync(file);
+    var jsonContent = JSON.parse(fileContents);
+    result = jsonContent;
+  } catch (err) {
+    result = false;
+  }
+  return result;
 };
--- src/bin/read-json-file.js
+++ src/bin/read-json-file.js
@@ -1,7 +1,13 @@
 import fs from 'fs';
 
 export default (file) => {
-  const fileContents = fs.readFileSync(file);
-  const jsonContent = JSON.parse(fileContents);
-  return jsonContent;
+  let result;
+  try {
+    const fileContents = fs.readFileSync(file);
+    const jsonContent = JSON.parse(fileContents);
+    result = jsonContent;
+  } catch (err) {
+    result = false;
+  }
+  return result;
 };
